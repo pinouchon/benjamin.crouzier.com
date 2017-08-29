9d91fc22fa9a8e956bde216dba9536975637348b
pinouchon <pinouchon@gmail.com>
2017-08-29 15:26:42 +0000

    change tagline

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
+
+  #match '/codemonkey' => redirect {|params, req| "https://www.linkedin.com/in/gearnode/"},  via: [:get, :post]
 end
